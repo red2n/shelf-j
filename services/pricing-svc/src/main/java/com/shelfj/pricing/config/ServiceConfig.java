3ce048489f8c4b801b3df20e653ba7475fa4d6c5
red2n <navinkumar.net@zohomail.in>
2026-06-09 18:55:52 +0000

    port issues

--- services/pricing-svc/src/main/java/com/shelfj/pricing/config/ServiceConfig.java
+++ services/pricing-svc/src/main/java/com/shelfj/pricing/config/ServiceConfig.java
@@ -2,9 +2,15 @@ package com.shelfj.pricing.config;
 
 import com.shelfj.service.BaseServiceConfig;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @ApplicationScoped
 public class ServiceConfig extends BaseServiceConfig {
+
+  @Inject
+  @ConfigProperty(name = "server.port", defaultValue = "8005")
+  int servicePort;
 
   @Override
   public String serviceName() {
@@ -13,7 +19,7 @@ public class ServiceConfig extends BaseServiceConfig {
 
   @Override
   public int servicePort() {
-    return 8005;
+    return servicePort;
   }
 
   @Override
--- services/purchase-svc/src/main/java/com/shelfj/purchase/config/ServiceConfig.java
+++ services/purchase-svc/src/main/java/com/shelfj/purchase/config/ServiceConfig.java
@@ -2,9 +2,16 @@ package com.shelfj.purchase.config;
 
 import com.shelfj.service.BaseServiceConfig;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @ApplicationScoped
 public class ServiceConfig extends BaseServiceConfig {
+
+  @Inject
+  @ConfigProperty(name = "server.port", defaultValue = "8009")
+  int servicePort;
+
   @Override
   public String serviceName() {
     return "purchase-svc";
@@ -12,7 +19,7 @@ public class ServiceConfig extends BaseServiceConfig {
 
   @Override
   public int servicePort() {
-    return 8009;
+    return servicePort;
   }
 
   @Override
